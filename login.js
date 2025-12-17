5ebebf879ebde91fc5c1534c77f832c79c86db55
NITESH KUMAR VERMA <kumarvermanitesh54@gmail.com>
2025-12-17 19:46:14 +0530

    js add

--- login.js
+++ login.js
@@ -2,5 +2,5 @@ document.getElementById("loginBtn").addEventListener("click", function(event) {
     event.preventDefault();  // Form submit hone se rokta hai
 
     // Yaha tum apna next page ka naam likho
-    window.location.href = "index.html";
+    window.location.href = "home.html";
 });
